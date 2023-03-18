36a762c7d4117f44cf122b479d851993aa73e303
Austine A <8253869+AustineA@users.noreply.github.com>
2023-03-18 06:45:27 +0100

    Font setup

---
+++ components/content.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+const Content = ({ children }) => {
+  return <PageContent>{children}</PageContent>;
+};
+
+export default Content;
+
+const PageContent = styled.section`
+  padding: 1.5rem;
+  color: var(--white);
+`;
