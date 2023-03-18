e17d5117a902845eb92d0fadb9925de6eb4c23e7
Austine A <8253869+AustineA@users.noreply.github.com>
2023-03-18 07:40:52 +0100

    Status complete

--- components/status.tsx
+++ components/status.tsx
@@ -1,5 +1,37 @@
+import { format } from "date-fns";
+import { useState } from "react";
+import styled from "styled-components";
+
 const Status = () => {
-  return <div>Status</div>;
+  const [day, _] = useState(new Date());
+  const currentHour = day.getHours();
+
+  return (
+    <StatusPage>
+      <span>{format(day, "eeee")}</span>
+      <div className="date">{format(day, "dd MMMM")}</div>
+      <p>
+        {currentHour > 17
+          ? "What did you work on today? "
+          : "What are working on today?"}
+      </p>
+    </StatusPage>
+  );
 };
 
 export default Status;
+
+const StatusPage = styled.div`
+  span {
+    color: var(--primary);
+  }
+
+  p {
+    font-weight: 200;
+  }
+
+  .date {
+    font-size: 4rem;
+    font-weight: 500;
+  }
+`;
