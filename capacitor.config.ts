dc926180aa52b65010ca6f17abf621475b0390ab
Austine A <8253869+AustineA@users.noreply.github.com>
2023-03-18 10:51:43 +0100

    Added capacitor and ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "xyz.lunabase.ace",
+  appName: "ace",
+  webDir: "build",
+  bundledWebRuntime: false,
+};
+
+export default config;
